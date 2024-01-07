6971ce307974acc008071878f26765f70467f606
Benny Chrispin <ndizibaidu23@gmail.com>
2024-01-07 22:35:23 +0200

    Update Home page and update features

--- Frontend/src/components/Feature.jsx
+++ Frontend/src/components/Feature.jsx
@@ -1,9 +1,12 @@
+import feature1 from "../assets/images/ZZM_6332.jpg";
+import feature2 from "../assets/images/ZZM_6047.jpg";
+
 export default function Feature() {
   return (
     <div className=" my-16 md:px-14 px-4 max-w-screen-2xl mx-auto">
       <div className=" flex flex-col lg:flex-row justify-between items-start gap-10">
         <div className=" lg:w-2/4">
-          <h3 className=" text-3xl font-bold lg:w-1/2 mb-3">
+          <h3 className="text-3xl font-bold lg:w-full mb-3">
             Get the help YOU need
           </h3>
           <p className=" text-base text-gray-500 font-normal leading-relaxed">
@@ -18,9 +21,18 @@ export default function Feature() {
             to help single parents better their financial, personal, and
             emotional well-being.
           </p>
+          <div className=" mt-7">
+            <img src={feature1} alt="features1" className="rounded-lg" />
+          </div>
         </div>
         {/* image for about */}
-        <div>Right Side</div>
+        <div className="w-full lg:w-3/4">
+          <div>
+            <div>
+              <img src={feature2} alt="features1" className="rounded-lg" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,14 +1,41 @@
+/* eslint-disable no-undef */
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css/navigation";
 import "swiper/css";
 import { IoIosArrowRoundForward, IoIosArrowRoundBack } from "react-icons/io";
-import cover1 from "../assets/images/ZZM_6301.jpg";
-import cover2 from "../assets/images/ZZM_5981.jpg";
-import cover3 from "../assets/images/ZZM_6156.jpg";
-import cover4 from "../assets/images/ZZM_6002.jpg";
-import cover5 from "../assets/images/ZZM_6036.jpg";
+
+import background1 from "../assets/images/background2.jpg";
+
 export default function Home() {
+  const backgroundImageUrl = `url(https://lh3.googleusercontent.com/pw/ABLVV84E_itgbVaqBfzJDhnT3EOUhBExokL-_IMytu-QazaLQ0QcJ1Ci_qXDFYZiq9-85sh6iM865r4YRx54_Ovgln4iCfRSrT4LbBZkt8F6d6cQUV7TIocSUormxhRZur2NYKjmEny0DqORWBdDsCsVgKs=w2334-h1558-s-no-gm?authuser=0)`;
+  const backgroundImageUrl1 = `url(${background1})`;
+  const backgroundImageUrl2 = `url(https://lh3.googleusercontent.com/pw/ABLVV859WuiICwSwCahER6yvWG37Dr_v4vap-qpWYMFiGJdUKDLrtulGeNEIplb26VeqVkisqMOJftGoZOuHnGcW8N3Ke4pYGhZ1Rok3sQmV_eCGYVUZDBBMQJZ06bKQPZEwCbcmtHfuBwPKd67O_gQp5rQ=w2334-h1558-s-no-gm?authuser=0)`;
+
+  const containerStyle = {
+    backgroundImage: backgroundImageUrl,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "550px",
+  };
+
+  const containerStyle1 = {
+    backgroundImage: backgroundImageUrl1,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "550px",
+  };
+
+  const containerStyle2 = {
+    backgroundImage: backgroundImageUrl2,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "550px",
+  };
+
   return (
     <>
       <Swiper
@@ -22,148 +49,97 @@ export default function Home() {
         modules={[Navigation]}
       >
         <SwiperSlide>
-          <div className="md:px-12 p-4 max-w-screen-3xl mx-auto mt-[90px] bg-black">
-            <div className=" rounded-xl rounded-br-[150px] gradientBg md:p-9 px-4 py-9">
-              <div className=" flex flex-col md:flex-row-reverse justify-between items-center gap-2">
-                {/* banner image */}
-                <div className="flex">
-                  <div className=" flex items-center mt-20">
-                    <img
-                      src={cover1}
-                      alt="Image"
-                      className="h-[386px] w-96 md:w-72 object-cover shadow-lg rounded-md"
-                    />
-                  </div>
-                  <div className="flex">
-                    <img
-                      src={cover2}
-                      alt="Image"
-                      className="w-96 h-[386px] md:w-72 object-cover shadow-lg rounded-md"
-                    />
-                  </div>
-                </div>
-                {/* banner content */}
-                <div className=" md:w-3/5 text-white">
-                  <h2 className=" lg:text-7xl text-4xl font-bold text-white mb-6">
-                    We are The Single Parent Project
-                  </h2>
-                  <div className=" text-[#EBEBEB] text-xl mb-8 leading-relaxed">
-                    <p>
-                      we are dedicated to providing support, stability, and
-                      community for single Parent families
-                    </p>
-                    <p>Help us Restore Single Parent Families in need.</p>
-                  </div>
+          <div
+            style={containerStyle}
+            className=" px-16 max-w-screen-3xl mx-auto mt-[90px]"
+          >
+            <div className="flex items-start justify-start py-32">
+              <div className=" flx-cols w-[350px]">
+                <h2 className="text-3xl font-bold text-white mb-6 border-b">
+                  We are The Single Parent Project
+                </h2>
 
-                  <button className=" py-2 px-8 bg-gray-300 rounded-lg text-black font-semibold cursor-pointer hover:bg-white transition-all duration-300">
-                    Make a Donation
-                  </button>
-                </div>
+                <p className=" text-white font-medium text-base lg:text-lg">
+                  Empowering Single Mothers to cope with the impact of divorce;{" "}
+                  <span className="font-bold">
+                    through Psychological Counseling, Promote Positive Parenting
+                    and Financial couching.
+                  </span>{" "}
+                </p>
+                <button className=" flex items-center gap-2 py-2 px-8 border-2 border-gray-100 shadow-lg rounded-lg text-gray-100 font-bold cursor-pointer hover:bg-white hover:text-stone-950 transition-all duration-300 mt-10">
+                  Make a Donate
+                </button>
               </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          {" "}
-          <div className="md:px-12 p-4 max-w-screen-3xl mx-auto mt-[90px] bg-black">
-            <div>
-              <div className=" flex flex-col md:flex-row-reverse justify-between items-center gap-2">
-                {/* banner image */}
-
-                <div className=" flex items-center mt-20">
-                  <img
-                    src={cover3}
-                    alt="Image"
-                    className="h-[386px] w-96 md:w-72 object-cover shadow-lg  rounded-t-full"
-                  />
-                </div>
-
-                {/* banner content */}
-                <div className=" md:w-3/5 text-white">
-                  <div className=" w-[250px] lg:w-[500px]">
-                    <h2 className="lg:text-7xl text-4xl font-bold text-white mb-6 border-b-2">
-                      Our Mission
-                    </h2>
-                  </div>
-
-                  <p className=" text-gray-100 font-normal text-base lg:text-2xl leading-7">
-                    Empowering Single Mothers to cope with the impact of
-                    divorce;{" "}
-                    <span className=" font-bold">
-                      through Psychological Counseling, Promote Positive
-                      Parenting and Financial couching.
-                    </span>{" "}
-                  </p>
-
-                  <button className=" flex items-center gap-2 py-2 px-8 bg-gray-200 rounded-lg text-black font-semibold cursor-pointer hover:bg-white transition-all duration-300 mt-10">
-                    Learn More{" "}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </button>
-                </div>
+          <div
+            style={containerStyle1}
+            className=" px-16 max-w-screen-3xl mx-auto mt-[90px]"
+          >
+            <div className="flex items-start justify-start py-32">
+              <div className=" flx-cols w-[250px]">
+                <h3 className="text-3xl font-bold text-teal-600 mb-6 border-b">
+                  Join Us In Making a Difference
+                </h3>
+                <p className="font-medium text-base lg:text-lg">
+                  Become part of our community by donating or volunteering your
+                  time.
+                </p>
+                <button className="flex items-center gap-2 py-2 px-8 border-2 border-black shadow-lg rounded-lg text-black font-bold cursor-pointer hover:bg-black hover:text-white transition-all duration-300 mt-10">
+                  Learn More
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
               </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          {" "}
-          <div className="md:px-12 p-4 max-w-screen-3xl mx-auto mt-[90px] bg-black">
-            <div className=" rounded-xl rounded-br-[150px] gradientBg md:p-9 px-4 py-9">
-              <div className=" flex flex-col md:flex-row-reverse justify-between items-center gap-2">
-                {/* banner image */}
-                <div className="flex">
-                  <div className=" flex items-center mt-20">
-                    <img
-                      src={cover5}
-                      alt="Image"
-                      className="h-[386px] w-96 md:w-72 object-cover shadow-lg rounded-xl"
+          <div
+            style={containerStyle2}
+            className=" px-16 max-w-screen-3xl mx-auto mt-[90px]"
+          >
+            <div className="flex items-start justify-start py-32">
+              <div className=" flx-cols w-[250px]">
+                <h3 className="text-3xl font-bold  mb-6 border-b">
+                  Get Involved with Our Cause
+                </h3>
+                <p className="font-medium text-base lg:text-lg text-white">
+                  See how you can contribute to our mission and make a real
+                  difference in the lives of those we serve.
+                </p>
+                <button className=" flex items-center gap-2 py-2 px-8 border-2 border-white shadow-lg rounded-lg text-white font-bold cursor-pointer hover:bg-white hover:text-stone-950 transition-all duration-300 mt-10">
+                  Our Impact
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z"
+                      clipRule="evenodd"
                     />
-                  </div>
-                  <div className="flex">
-                    <img
-                      src={cover4}
-                      alt="Image"
-                      className="w-96 h-[386px] md:w-72 object-cover shadow-lg rounded-xl"
-                    />
-                  </div>
-                </div>
-                {/* banner content */}
-                <div className=" md:w-3/5 text-white">
-                  <h2 className=" lg:text-7xl text-4xl font-bold text-white mb-6">
-                    Our Values
-                  </h2>
-                  <h3 className=" font-semibold leading-7 text-xl mb-4">
-                    Compassion. Community.Service.Respect.Equality.
-                  </h3>
-
-                  <p className="text-[#EBEBEB] lg:text-lg mb-8 leading-relaxed">
-                    we value the well-being of others. We recognize and
-                    appreciate our community and strive to give back. We aim to
-                    address the public &apos; s needs and value the opportunity
-                    to be a positive solution for change. We admire the
-                    difference and individuality of each person &apos; s
-                    strengths, weakness, and perspective. we accept all. All are
-                    welcome. All are loved
-                  </p>
-
-                  <button className=" py-2 px-8 bg-gray-300 rounded-lg text-black font-semibold cursor-pointer hover:bg-white transition-all duration-300">
-                    Our Impact
-                  </button>
-                </div>
+                  </svg>
+                </button>
               </div>
             </div>
           </div>
         </SwiperSlide>
+
         <div className="absolute top-[50%] z-10 -left-[23rem] text-black w-[40px] h-[40px] bg-white grid place-items-center cursor-pointer button-prev-slide group-hover:left-0 font-semibold rounded-full">
           <IoIosArrowRoundBack />
         </div>
