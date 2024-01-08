fb74e211ec93ac9b8ceadcdcca7502fd06da50c4
Benny Chrispin <ndizibaidu23@gmail.com>
2024-01-09 00:06:14 +0200

    complete Our Family

--- Frontend/src/components/Our-Family.jsx
+++ Frontend/src/components/Our-Family.jsx
@@ -1,16 +1,19 @@
 export default function OurFamily() {
   return (
-    <div className=" flex-col max-w-screen-3xl mx-auto mt-6 items-center justify-center text-center w-[600px]">
+    <div className=" flex-col max-w-screen-3xl mx-auto mt-6 items-center justify-center text-center w-[600px] lg:w-[800px]">
       <h1 className=" text-2xl font-semibold text-black">
         Your Donation Empower Our Families
       </h1>
-      <p className=" text-gray-500">
-        It’s already February and we need your support more than ever. Our
-        single parent families have been impacted over the last two years badly.
-        They need your help. Together, we can raise awareness, funds, and
-        resources to ensure our single parent families have everything they need
-        to grow and thrive in 2022!
+      <p className=" text-gray-500 py-3">
+        It’s already January and we need your support more than ever. Our single
+        parent families have been impacted over the last two years badly. They
+        need your help. Together, we can raise awareness, funds, and resources
+        to ensure our single parent families have everything they need to grow
+        and thrive in 2024!
       </p>
+      <button className="p-3 bg-orange-500 mt-4 rounded-lg font-semibold hover:bg-orange-600 transition-all duration-500 w-[220px] mb-3 uppercase">
+        Donate Now
+      </button>
     </div>
   );
 }
