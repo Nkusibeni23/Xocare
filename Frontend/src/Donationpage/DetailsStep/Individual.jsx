944a2192c716e63461330a2e0ec661a6d8ab9712
Your GitHub Username <Your GitHub Email>
2024-02-22 00:42:20 +0200

    finish individual page

--- Frontend/src/Donationpage/DetailsStep/Individual.jsx
+++ Frontend/src/Donationpage/DetailsStep/Individual.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 import { Country, City } from "country-state-city";
 import { useEffect, useState } from "react";
+import "react-phone-input-2/lib/style.css"; // Import the styles
+import PhoneInput from "react-phone-input-2";
+import "react-phone-number-input/style.css";
 export default function Individual() {
   const countryData = Country.getAllCountries();
   const [selectedCountry, setSelectedCountry] = useState("");
   const [selectedCityDropdown, setSelectedCityDropdown] = useState("");
   const [cities, setCities] = useState([]);
   const [selectedCityInput, setSelectedCityInput] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("+250");
 
   useEffect(() => {
     if (selectedCountry) {
@@ -22,18 +26,18 @@ export default function Individual() {
 
   const handleCountryChange = (e) => {
     setSelectedCountry(e.target.value);
-    setSelectedCityDropdown(""); // Reset selected city for dropdown
-    setSelectedCityInput(""); // Reset selected city for input
+    setSelectedCityDropdown("");
+    setSelectedCityInput("");
   };
 
   const handleCityDropdownChange = (e) => {
     setSelectedCityDropdown(e.target.value);
-    setSelectedCityInput(e.target.value); // Update input value
+    setSelectedCityInput(e.target.value);
   };
 
   const handleCityInputChange = (e) => {
     setSelectedCityInput(e.target.value);
-    setSelectedCityDropdown(""); // Reset selected city for dropdown
+    setSelectedCityDropdown("");
   };
   return (
     <div>
@@ -67,8 +71,12 @@ export default function Individual() {
           <input
             className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
             placeholder="Email"
-            type="text"
+            type="email"
           />
+          <p className=" text-sm text-gray-500 text-center">
+            Your receipt will be sent to this email.Make sure to double-check
+            for typos.
+          </p>
           <input
             className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
             placeholder="Address"
@@ -85,7 +93,7 @@ export default function Individual() {
             </span>
           </div>
           <select
-            className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
+            className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30 cursor-pointer"
             value={selectedCountry}
             onChange={handleCountryChange}
             style={{ width: "100%" }}
@@ -99,39 +107,54 @@ export default function Individual() {
               </option>
             ))}
           </select>
-          <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
-            <select
-              className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
-              value={selectedCityDropdown}
-              onChange={handleCityDropdownChange}
-              disabled={!selectedCountry}
-              style={{ width: "100%" }}
-            >
-              <option value="" disabled>
-                Select City
+          <select
+            className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30 cursor-pointer"
+            value={selectedCityDropdown}
+            onChange={handleCityDropdownChange}
+            disabled={!selectedCountry}
+            style={{ width: "100%" }}
+          >
+            <option value="" disabled>
+              Select City
+            </option>
+            {cities.map((city) => (
+              <option key={city.name} value={city.name}>
+                {city.name}
               </option>
-              {cities.map((city) => (
-                <option key={city.name} value={city.name}>
-                  {city.name}
-                </option>
-              ))}
-            </select>
-            <input
-              className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
-              placeholder="State"
-              type="text"
-            />
-            <input
-              className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
-              placeholder="Zip Code"
-              type="text"
-            />
+            ))}
+          </select>
+          <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
+            <div className="relative">
+              <input
+                className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
+                placeholder="State"
+                type="text"
+              />
+              <span className=" absolute top-7 text-gray-500 right-5">
+                Optional
+              </span>
+            </div>
+            <div>
+              <input
+                className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30 "
+                placeholder="Zip Code"
+                type="text"
+              />
+            </div>
           </div>
 
-          <input
-            className="w-full px-5 py-3 mt-4 border-2 rounded-lg placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
-            placeholder="Phone"
-            type="text"
+          <PhoneInput
+            className="py-3 mt-4 rounded-lg border-2 placeholder-gray-500 bg-white text-gray-800 border-gray-700 focus:border-gray-900 focus:ring-gray-900 focus:outline-none focus:ring focus:ring-opacity-30"
+            defaultCountry="RW"
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChange={(value) => setPhoneNumber(value)}
+            inputStyle={{
+              border: "none",
+              boxShadow: "none",
+              outline: "none",
+              borderColor: "transparent",
+            }}
           />
         </div>
       </form>
