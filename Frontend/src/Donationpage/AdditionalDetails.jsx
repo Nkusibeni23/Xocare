8cb4afb48a1ac6fdddee1c499f52420d442932d6
Your GitHub Username <Your GitHub Email>
2024-02-22 21:25:37 +0200

    fix additional phase

--- Frontend/src/Donationpage/AdditionalDetails.jsx
+++ Frontend/src/Donationpage/AdditionalDetails.jsx
@@ -1,24 +1,19 @@
 export default function AdditionalDetails() {
   return (
-    <div className=" mx-auto max-w-screen-2xl">
+    <div className="">
       <p className="text-base text-gray-500 -mt-16 flex items-center text-center justify-center">
         Just a few more questions!
       </p>
-      <div className="flex-1 justify-center items-center w-full bg-white mt-4">
-        <div className="flex justify-between">
-          <div className="mb-3">
-            <span className="font-semibold cursor-pointer p-2">
-              Write us a comment
-            </span>
-          </div>
-        </div>
-        <div className="relative">
-          <textarea
-            className="p-4 focus:outline-gray-700 focus:outline-offset-1 border-2 resize-none h-[120px] border-[#9EA5B1] rounded-lg w-[60vw] px-4"
-            placeholder="Add your comment..."
-          ></textarea>
-          <span className="absolute top-5 text-gray-500 right-4">Optional</span>
-        </div>
+      <div className="mb-1 mt-6 block">
+        <label className="block font-semibold cursor-pointer p-2">
+          Write us a comment
+        </label>
+        <textarea
+          className=" border focus:outline-2 focus:outline-gray-600 rounded-lg border-gray-500 p-4 placeholder:text-gray-500 text-gray-950 transition-all duration-300"
+          placeholder="Just a few more questions for details here..."
+          rows="5"
+          cols="70"
+        />
       </div>
     </div>
   );
