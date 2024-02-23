de073503aa6182ef9f2ff2ce8e9cdb776de11fe5
Your GitHub Username <Your GitHub Email>
2024-02-23 11:01:59 +0200

    some changes

--- Frontend/src/Donationpage/AdditionalDetails.jsx
+++ Frontend/src/Donationpage/AdditionalDetails.jsx
@@ -5,15 +5,18 @@ export default function AdditionalDetails() {
         Just a few more questions!
       </p>
       <div className="mt-6 block">
-        <label className="block font-medium cursor-pointer p-2">
-          Write us a comment
-        </label>
+        <label className="block font-medium p-2">Write us a comment</label>
         <textarea
           className="border-2 focus:outline-4 focus:outline-gray-800 rounded-xl border-gray-500 p-4 placeholder:text-gray-500 text-gray-950 transition-all duration-300 focus:shadow-lg"
           placeholder="Just a few more questions..."
           rows="5"
           cols="80"
         />
+        <div className="relative">
+          <span className="absolute bottom-28 right-8 text-gray-500">
+            Optional
+          </span>
+        </div>
       </div>
     </div>
   );
